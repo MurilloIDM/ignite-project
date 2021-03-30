bb1a4150496106a6c5b9995351dda905856256d7
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2021-03-30 06:55:43 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
+
+app.listen(3000, () => console.log("Server is running!"));
