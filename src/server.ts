db000f16ffcc7b0a15ef86586cdc049cdddb92aa
MurilloIDM <murillo.idem@gmail.com>
2021-04-30 08:04:16 -0300

    Organizando a rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRouter } from "./routes/Specification.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationRouter);
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running!"));
