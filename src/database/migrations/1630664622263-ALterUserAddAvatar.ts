c5718321d1dabc4d77dbef5c3abdfbfaf4c6c1a8
MurilloIDM <murillo.idem@gmail.com>
2021-09-03 21:52:17 -0300

    Configuração do upload de avatar

---
+++ src/database/migrations/1630664622263-ALterUserAddAvatar.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class ALterUserAddAvatar1630664622263 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "avatar",
+        type: "varchar",
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "avatar");
+  }
+}
