5fcc6aa741ff64afec724f4b6a0db0a8b1d60ae5
MurilloIDM <murillo.idem@gmail.com>
2022-01-06 08:24:01 -0300

    New: implementação de testes para AuthenticateUser

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,76 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUserCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUserCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      email: "user@teste.com",
+      password: "1234",
+      name: "User Test",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUserCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate an nonexistent user", () => {
+    const execute = async () => {
+      await authenticateUserUserCase.execute({
+        email: "false@email.com",
+        password: "1234",
+      });
+    };
+
+    expect(execute).rejects.toBeInstanceOf(AppError);
+    expect(execute).rejects.toEqual({
+      message: "Email or password incorrect!",
+      statusCode: 400,
+    });
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+    const execute = async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "9999",
+        email: "user@user.com",
+        password: "1234",
+        name: "User Test Error",
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUserCase.execute({
+        email: user.email,
+        password: "incorrectPassword",
+      });
+    };
+
+    expect(execute).rejects.toBeInstanceOf(AppError);
+    expect(execute).rejects.toEqual({
+      message: "Email or password incorrect!",
+      statusCode: 400,
+    });
+  });
+});
