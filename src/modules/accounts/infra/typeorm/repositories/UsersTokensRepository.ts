32b3e6c4549a5f6de68e6c2521a734751c300f57
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-03-18 06:47:55 -0300

    New: Repositório de Refresh Token

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userTokens = this.repository.create({
+      user_id,
+      expires_date,
+      refresh_token,
+    });
+
+    await this.repository.save(userTokens);
+
+    return userTokens;
+  }
+}
+
+export { UsersTokensRepository };
