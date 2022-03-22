9bd40a90e89c890af0dba78f86328169069ec1a9
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-03-22 07:08:36 -0300

    New: criando caso de uso para recuperação de senha

--- src/shared/container/providers/DateProvider/implementations/DayjsProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsProvider.ts
@@ -29,6 +29,10 @@ class DayjsProvider implements IDateProvider {
   addDays(days: number): Date {
     return dayjs().add(days, "days").toDate();
   }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hour").toDate();
+  }
 }
 
 export { DayjsProvider };
