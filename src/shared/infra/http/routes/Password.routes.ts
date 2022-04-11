515bfce5b00ffd6079bb80c9b20cd0775c02cc4a
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-04-11 07:34:05 -0300

    New: Caso de uso de reset da senha

--- src/shared/infra/http/routes/Password.routes.ts
+++ src/shared/infra/http/routes/Password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRouter = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordController = new ResetPasswordUserController();
 
 passwordRouter.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRouter.post("/reset", resetPasswordController.handle);
 
 export { passwordRouter };
