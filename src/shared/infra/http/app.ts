ddcc6b0c85bfe3fd5f08429843bb5e2e955cd3ce
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-04-14 07:19:32 -0300

    New: Add Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,16 +8,33 @@ import { router } from "@shared/infra/http/routes";
 import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import swaggerFile from "../../../swagger.json";
 
 createConnection();
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, _request: Request, response: Response, next: NextFunction) => {
